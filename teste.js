1c0930e0404c0c62a91432c473a2657b64bcce85
Gufonteszm <fontesmazzagustavo320@gmail.com>
2026-03-02 08:40:18 -0300

    aula02

---
+++ teste.js
@@ -0,0 +1,11 @@
+let resposta = confirm("voce deseja continuar?");
+if (resposta) {
+alert("voce escolheu continuar.");
+} else {
+alert ("voce cancelou a ação");
+
+}
+
+let nome = prompt ("qual é o seu nome");
+alert ("ola, " + nome + "!");
+
